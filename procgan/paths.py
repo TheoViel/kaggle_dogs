fad95c096a15ee94ff912bb8fbfe70bbcab88009
TheoViel <theo.viel@eleves.enpc.fr>
2019-08-28 17:04:43 +0200

    added weights and metric graph

--- procgan/paths.py
+++ procgan/paths.py
@@ -1,6 +1,6 @@
 IMAGE_FOLDER = '../input/all-dogs'
 ANNOTATION_PATH = '../input/annotation/Annotation'
 DATA_PATH = '../input/all-dogs/all-dogs/'
-WEIGHTS_PATH = '../input/dog-face-generation-competition-kid-metric-input/classify_image_graph_def.pb'
+WEIGHTS_PATH = '../input/mifid_graph/classify_image_graph_def.pb'
 CP_PATH = "../output/weights/"
 IMG_PATH = "../output/images/"
